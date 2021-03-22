cb16cbe75d80f568cc9821e5a69dfacc13897bbf
Thiago Bruno <tyagobruno@live.com>
2021-03-22 18:36:35 -0300

    fix types

---
+++ @types/index.d.ts
@@ -0,0 +1,12 @@
+// THEME PROVIDER
+export {ThemeProvider, useTheme} from '../src/ThemeProvider';
+export {DefaultTheme} from '../src/ThemeProvider/theme';
+
+// COMPONENTS
+export {default as Title} from '../src/Title';
+export {default as Paragraph} from '../src/Paragraph';
+export {default as Button} from '../src/Button';
+export {default as Spacing} from '../src/Spacing';
+export {default as Label} from '../src/Label';
+export {default as IconButton} from '../src/IconButton';
+export {default as AppBar, AppBarIcon} from '../src/AppBar';
